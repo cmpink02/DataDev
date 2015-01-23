391f97469c46d87999b39490c4d7ad029d7440f8
cmpink02 <cmpink02@users.noreply.github.com>
2015-01-23 16:23:22 -0500

    Updated the comments to be clearer and more accurate

--- server.R
+++ server.R
@@ -1,12 +1,13 @@
 library(shiny)
 
-# Rely on the 'WorldPhones' dataset in the datasets
+# Rely on the 'HairEyeColor' dataset in the datasets
 # package (which generally comes preloaded).
 library(datasets)
 
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
   
+  # Allow the inputs to determine which values of the HairEyeColor dataset will be used.
   dataInput <- reactive({
     if (input$sex == 'Both') {
       x <- as.table(apply(HairEyeColor, c(1,2), sum))
@@ -19,18 +20,20 @@ shinyServer(function(input, output) {
     }
     return(x)
   })
+  
   # Fill in the spot we created for a plot
+  # In this case, I decided to use the mosaicplot layout.
   output$hairEyePlot <- renderPlot({
     x<-dataInput()
     
     if (input$transpose == 'Eye Color') {
-      colorlist <- c("brown","blue","#594C26", "green")
+      colorlist <- c("brown","blue","#594C26", "green") #List of eye colors used to color the plot
       y_title = "Eye Color"
       x_title = "Hair Color"
     }
     else {
       x<-t(x)
-      colorlist <- c("black", "brown", "red", "#FAF0BE")
+      colorlist <- c("black", "brown", "red", "#FAF0BE") #List of hair colors used to color the plot
       y_title = "Hair Color"
       x_title = "Eye Color"
     }
@@ -43,11 +46,16 @@ shinyServer(function(input, output) {
     
   })
   
+  # This generates the text about the most/least common combinations.
   output$popularity <- renderText({
   
     y<-as.data.frame(dataInput())
+    
+    # Max --> These are the most common combinations; Pick the first in the event of a tie.
     hair <- y[which(y$Freq == max(y$Freq)), "Hair"][1]
     eye <- y[which(y$Freq == max(y$Freq)), "Eye"][1]
+    
+    # Min --> These are the least common combinations; Pick the first in the event of a tie.
     hair2 <- y[which(y$Freq == min(y$Freq)), "Hair"][1]
     eye2 <- y[which(y$Freq == min(y$Freq)), "Eye"][1]
     
